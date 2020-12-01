673a2b1c66dbae386cd351864b9bce91f83be284
Elizabeth McCracken <39070922+eemcc@users.noreply.github.com>
2020-12-01 10:06:30 -0500

    Update search.js
    updated ui

--- search.js
+++ search.js
@@ -1,29 +1,31 @@
-$(".univ-search-input").click(function() {
-  $(".drawer").fadeIn();
+$(".search").click(function() {
+  $(".drawer").show();
 });
 
-const client = algoliasearch('3J2D8ITRRP', '7aaf1b8db0ab4f0de35c2ed6649efeb5');
-const search = client.initIndex('dev_MarketingSite');
+if (!window.losantAlgoliaSearchClient && !window.search) {
+  window.losantAlgoliaSearchClient = algoliasearch('3J2D8ITRRP', '7aaf1b8db0ab4f0de35c2ed6649efeb5');
+  window.search = losantAlgoliaSearchClient.initIndex('dev_MarketingSite');
+}
 
 autocomplete(
-  '#univ-search-input',
+  '#univ-search-input-desktop',
   {
     debug: true,
     minLength: 2,
   },
   [
     {
-      source: autocomplete.sources.hits(search, {hitsPerPage: 10}),
+      source: autocomplete.sources.hits(window.search, {hitsPerPage: 10}),
         displayKey: 'Losant',
         name: 'title',
         templates: {
-          suggestion({absoluteURL, _highlightResult, _snippetResult, type}) {
+          suggestion({url, _highlightResult, _snippetResult, type}) {
             return `
-            <a href="${absoluteURL}">
+            <a href="${url}">
               <table>
                 <tr>
                   <th>
-                    <h4>${_highlightResult.header.value} <span style="color: #FF495C;">|</span> ${type}</h4>
+                    <h4 style="text-transform: capitalize;">${_highlightResult.header.value} <span style="color: #FF495C;">|</span> ${type}</h4>
                   </th>
                   <th>
                     <img src="https://f.hubspotusercontent40.net/hubfs/742943/Website/Images/Icons/ios-arrow-forward.svg" width="30px"; height="30px";>
@@ -37,3 +39,38 @@ autocomplete(
     }
   ]
 );
+
+autocomplete(
+  '#univ-search-input-mobile',
+  {
+    debug: true,
+    minLength: 2,
+  },
+  [
+    {
+      source: autocomplete.sources.hits(window.search, {hitsPerPage: 10}),
+        displayKey: 'Losant',
+        name: 'title',
+        templates: {
+          suggestion({url, _highlightResult, _snippetResult, type}) {
+            return `
+            <a href="${url}">
+              <table>
+                <tr>
+                  <th>
+                    <h4 style="text-transform: capitalize;">${_highlightResult.header.value} <span style="color: #FF495C;">|</span> ${type}</h4>
+                  </th>
+                  <th>
+                    <img src="https://f.hubspotusercontent40.net/hubfs/742943/Website/Images/Icons/ios-arrow-forward.svg" width="30px"; height="30px";>
+                  </th>
+                </tr>
+              </table>
+              <p>${_snippetResult.body.value}</p>
+              </a>`;
+          },
+        },
+    }
+  ]
+);
+
+
